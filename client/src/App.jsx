3d348d8c2da63b8097257a310b77cc38781b35aa
salih-dot-oss <tafzombaye11@gmail.com>
2026-05-21 02:33:17 +0000

    Fix: retirer les routes CV non commitées pour corriger le build Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import PortfolioPage from './pages/PortfolioPage'
 import AdminPage    from './admin/AdminPage'
-import CVPage       from './pages/CVPage'
-import CVDevPage    from './pages/CVDevPage'
 
 export default function App() {
   return (
@@ -12,8 +10,6 @@ export default function App() {
         <Routes>
           <Route path="/"        element={<PortfolioPage />} />
           <Route path="/admin/*" element={<AdminPage />} />
-          <Route path="/cv"      element={<CVPage />} />
-          <Route path="/cv-dev"  element={<CVDevPage />} />
         </Routes>
       </Router>
     </ThemeProvider>
